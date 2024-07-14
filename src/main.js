50008ba33f5f5f0b6f657b918e1d60590813db84
Hector Duarte <hectorduarte240.hd@gmail.com>
2024-07-14 13:31:32 -0300

    Menu Hamburger

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const closeIcon = document.getElementById("nav-close");
 
+closeIcon.addEventListener("click",() => {
+    navMenu.classList.add("hidden")
+})
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.remove("hidden")
+
+} )
 /*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
 
 /*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+    },
     extend: {
-      primaryColor: "#010a5e"
+      colors: {
+        primaryColor: "#010a5e",
+        primaryColorLigth: "#010d78",
+        secondaryColor: "#FFCC00",
+        paragrapColor: "#c0c0c0",
+        whiteColor: "#fff",
+        blackColor: "#000",
+        greenColor: "#007936",
+        redColor: "#cc3433",
+        darkColor: "#000",
+        darkColorLight: "#171717",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+      },
+    },
+    fontFamily: {
+      oswald: ["Oswand", "sans-serif"],
+      dmsans: ["DM Sans", "sans-serif"],
     },
   },
   plugins: [],
